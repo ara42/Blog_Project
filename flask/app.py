32194ef801acbcc8d14b26e8326d87fc31c69fdb
ara42 <136437583+ara42@users.noreply.github.com>
2023-08-21 17:42:03 +0900

    db방식 SQLAlchemy로 변경

--- flask/app.py
+++ flask/app.py
@@ -1,27 +1,39 @@
 from flask import Flask, request, render_template
 import pymysql
 import urllib.request
+from sqlalchemy import create_engine, MetaData, Table, select, func
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.automap import automap_base
 
 app = Flask(__name__)
 
 ## 한글 출력
 app.config['JSON_AS_ASCII'] = False
 
-## DB 연동
-mydb = pymysql.connect(
-    host="blogdb.cm2yxwfja9ii.ap-northeast-2.rds.amazonaws.com",
-    user="admin",
-    password="", ## 비밀번호 입력 필수
-    database="Naver_Blogs",
-    port=3306, 
-    charset="utf8")
+## DB 연동 (SQLAlchemy)
+user = "admin"
+password = "" ## 비밀번호 입력
+host = "blogdb.cm2yxwfja9ii.ap-northeast-2.rds.amazonaws.com"
+port = 3306
+db = "Naver_Blogs"
+db_url = f'mysql+pymysql://{user}:{password}@{host}:{port}/{db}'
 
-## DB 데이터 추출
-def db_result(sql):
-    mycursor = mydb.cursor()
-    mycursor.execute(sql)
-    results = mycursor.fetchall()
-    return results
+## 엔진 생성
+engine = create_engine(db_url, echo=False) ## SQL문 출력하지 않도록 설정
+
+## 기존 테이블을 매핑할 기본 클래스 생성
+Base = automap_base()
+
+## 엔진과 테이블 매핑
+Base.prepare(engine, reflect=True)
+
+## 테이블 클래스 가져오기
+restaurants = Base.classes.restaurants
+flask_exDB = Base.classes.flask_exDB
+
+## 세션 생성
+Session = sessionmaker(engine)
+session = Session()
 
 ## 첫 번째 페이지
 @app.route('/')
@@ -33,23 +45,15 @@ def home():
 @app.route('/place', methods=['GET', 'POST'])
 def place():
     location = request.form.get('location', '')
-    sql1 = f"SELECT rec_name FROM flask_exDB WHERE district = '{location}' ORDER BY `rank`"
-    restaurants = db_result(sql1)
+    stmt = select(flask_exDB.rec_name).where(flask_exDB.district == f'{location}')
+    restaurants = session.execute(stmt).all()
     return render_template('page2.html', name=location, restaurants=restaurants)
 
 ## 세 번째 페이지
 @app.route('/restaurant', methods=['GET', 'POST'])
 def restaurant():
-    store = request.form.get('restaurant', '')
-    img_folder = "/content/imgs/"
-
-    for i in data:
-        try:
-            url = i[1]
-            urllib.request.urlretrieve(url, img_folder+str(i[0])+".jpg")
-        except:
-            print(i)
-    return store
+    res = request.form.get('restaurant', '')
+    return render_template('page3.html', restaurant=res)
 
 if __name__ == '__main__':
     app.run(debug=True)
