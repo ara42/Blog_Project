5d2b80b55f3866b7d08ea6ac08ca9f610def6198
ara42 <136437583+ara42@users.noreply.github.com>
2023-08-22 14:14:21 +0900

    Update app.py
    page3에 이미지 출력 부분 수정

--- flask/app.py
+++ flask/app.py
@@ -28,12 +28,17 @@ Base = automap_base()
 Base.prepare(engine, reflect=True)
 
 ## 테이블 클래스 가져오기
-restaurants = Base.classes.restaurants
-flask_exDB = Base.classes.flask_exDB
+tb_res = Base.classes.restaurants
+tb_flask = Base.classes.flask_exDB
 
 ## 세션 생성
 Session = sessionmaker(engine)
 session = Session()
+
+## url로 사진 저장하는 함수
+def photo(img_url, img_name):
+    img_folder = "C:/work/python/blog_API/static/"
+    urllib.request.urlretrieve(img_url, img_folder+img_name)
 
 ## 첫 번째 페이지
 @app.route('/')
@@ -44,16 +49,28 @@ def home():
 ## 두 번째 페이지
 @app.route('/place', methods=['GET', 'POST'])
 def place():
-    location = request.form.get('location', '')
-    stmt = select(flask_exDB.rec_name).where(flask_exDB.district == f'{location}')
-    restaurants = session.execute(stmt).all()
+    location = request.form.get('location', '') ## HTML에서 location값 받아옴
+    sel_rec_name = select(tb_flask.rec_name).where(tb_flask.district == f'{location}') 
+    restaurants = session.execute(sel_rec_name).all() ## 받아온 location과 같은 식당이름 저장
     return render_template('page2.html', name=location, restaurants=restaurants)
 
 ## 세 번째 페이지
 @app.route('/restaurant', methods=['GET', 'POST'])
 def restaurant():
     res = request.form.get('restaurant', '')
-    return render_template('page3.html', restaurant=res)
+    
+    #sel_url = select(tb_flask.graph_url1).where(tb_flask.rec_name == f'{res}') 
+    
+    post_cnt = 10
+    l_img = []
+    sel_url = select(tb_flask.graph_url1).limit(post_cnt)
+    url = session.execute(sel_url).all()
+    for i in range(post_cnt):
+        img = f'img{i}.jpg'
+        l_img.append(img)
+        photo(url[i][0], img)
+    print(l_img)
+    return render_template('page3.html', restaurant=res, img_list=l_img)
 
 if __name__ == '__main__':
     app.run(debug=True)
