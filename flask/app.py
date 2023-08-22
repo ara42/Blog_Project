b75ea06c412ed6fb57897b3fdcf9d77c6513d134
ara42 <136437583+ara42@users.noreply.github.com>
2023-08-22 15:45:08 +0900

    Update app.py

--- flask/app.py
+++ flask/app.py
@@ -4,6 +4,8 @@ import urllib.request
 from sqlalchemy import create_engine, MetaData, Table, select, func
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.automap import automap_base
+import pygal
+from pygal.style import Style
 
 app = Flask(__name__)
 
@@ -52,7 +54,38 @@ def place():
     location = request.form.get('location', '') ## HTML에서 location값 받아옴
     sel_rec_name = select(tb_flask.rec_name).where(tb_flask.district == f'{location}') 
     restaurants = session.execute(sel_rec_name).all() ## 받아온 location과 같은 식당이름 저장
-    return render_template('page2.html', name=location, restaurants=restaurants)
+
+    custom_style = Style(
+        background='white',
+        plot_background='white',
+        font_family = 'sans-serif',
+        title_font_size = 45,
+        label_font_size = 35,
+        legend_font_size = 35
+    )
+
+    pie_chart = pygal.Pie(inner_radius=.4, style=custom_style)
+    pie_chart.title = '광고 비율'
+    pie_chart.add('IE', 19.5)
+    pie_chart.add('Firefox', 36.6)
+    pie_chart.add('Chrome', 36.3)
+    pie_chart.add('Safari', 4.5)
+    pie_chart.add('Opera', 2.3)
+    pie_chart = pie_chart.render_data_uri()
+
+    treemap = pygal.Treemap(style=custom_style)
+    treemap.title = '단어 군집화'
+    treemap.add('A', [2, 1, 12, 4, 2, 1, 1, 3, 12, 3, 4, None, 9])
+    treemap.add('B', [4, 2, 5, 10, 3, 4, 2, 7, 4, -10, None, 8, 3, 1])
+    treemap.add('C', [3, 8, 3, 3, 5, 3, 3, 5, 4, 12])
+    treemap.add('D', [23, 18])
+    treemap.add('E', [1, 2, 1, 2, 3, 3, 1, 2, 3, 4, 3, 1, 2, 1, 1, 1, 1, 1])
+    treemap.add('F', [31])
+    treemap.add('G', [5, 9.3, 8.1, 12, 4, 3, 2])
+    treemap.add('H', [12, 3, 3])
+    treemap = treemap.render_data_uri()
+
+    return render_template('page2.html', name=location, restaurants=restaurants, pie_chart=pie_chart, treemap=treemap)
 
 ## 세 번째 페이지
 @app.route('/restaurant', methods=['GET', 'POST'])
@@ -63,7 +96,7 @@ def restaurant():
     
     post_cnt = 10
     l_img = []
-    sel_url = select(tb_flask.graph_url1).limit(post_cnt)
+    sel_url = select(tb_flask.graph_url1,tb_flask.graph_url1,tb_flask.graph_url1).limit(post_cnt)
     url = session.execute(sel_url).all()
     for i in range(post_cnt):
         img = f'img{i}.jpg'
