941e30d15d3bc8c1c191a37f3450714b5d9fce51
ara42 <136437583+ara42@users.noreply.github.com>
2023-08-25 14:22:17 +0900

    Update ocr_ad.py

--- ml/ocr_ad.py
+++ ml/ocr_ad.py
@@ -1,3 +1,5 @@
+## 비밀번호 입력, 경로설정
+
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -8,7 +10,7 @@ import joblib
 
 blogdb = pymysql.connect(host='blogdb.cm2yxwfja9ii.ap-northeast-2.rds.amazonaws.com',
                       user='admin',
-                      password='blogdb!2',
+                      password='', ##비밀번호 입력
                       database='blogdb',
                       charset='utf8',
                       port=3306)
@@ -19,16 +21,29 @@ res_id1 = int(input("시작할 res_id : "))
 res_id2 = int(input("끝낼 res_id : "))
 
 # 데이터불러오기
-query = f"SELECT id, ocr_text FROM post_data WHERE map=1 AND ad_status = 'NEED-TO-PROCESS' AND res_id >= {res_id1} AND res_id <= {res_id2}"
-cursor.execute(query)
+sel_query = f"SELECT id, ocr_text FROM post_data WHERE map=1 AND ad_status = 'NEED-TO-PROCESS' AND images <> '' AND res_id >= {res_id1} AND res_id <= {res_id2}"
+#query = f"SELECT id, ocr_text FROM post_data WHERE map=1 AND ad_status = 'AD' AND ocr_text <> '' AND res_id >= {res_id1} AND res_id <= {res_id2}"
+cursor.execute(sel_query)
 results = cursor.fetchall()
 
-ad_tfidf = joblib.load('C:/work/python/blog_API/model/ad_tfidf.pkl') ## 경로 설정
-ad_svm = joblib.load("C:/work/python/blog_API/model/ad_svm.pkl") ## 경로 설정
+#print(results)
+
+ad_tfidf = joblib.load('C:/work/python/blog_API/model/tfidf_vectorizer6.pkl') ## 경로 설정
+ad_svm = joblib.load("C:/work/python/blog_API/model/svm_model6.pkl") ## 경로 설정
 
 for i in range(len(results)):
     ocr_text = results[i]['ocr_text']
     post_data_id = results[i]['id']
     x_new = ad_tfidf.transform([ocr_text])
-    predicted_label =ad_svm.predict(x_new)
-    print(post_data_id, predicted_label)
+    predicted_label = ad_svm.predict(x_new)
+    if predicted_label[0]:
+        ad = 'AD'
+    else:
+        ad = 'NON-AD'
+    up_query = f"update post_data set ad_status = %s where id = %s"
+    data = (ad, post_data_id)
+    cursor.execute(up_query, data)
+    print(post_data_id, ad)
+
+blogdb.commit()
+    
