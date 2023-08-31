30d0b0fae6f5b9c67c98912480a32840ba8d9f6c
ara42 <136437583+ara42@users.noreply.github.com>
2023-08-31 14:00:18 +0900

    final
    예외처리 추가

--- flask/app.py
+++ flask/app.py
@@ -16,6 +16,7 @@ import matplotlib
 import io
 import base64
 import json
+import threading
 
 matplotlib.use('Agg')
 
@@ -59,8 +60,7 @@ font_path = "C:/work/python/blog_API/static/bmjua.ttf"
 custom_style = Style(
     background='transparent',
     plot_background='white',
-    font_family = font_path,
-    title_font_family = font_path,
+    font_family = "NanumGothic",
     title_font_size = 30,
     label_font_size = 25,
     legend_font_size = 25,
@@ -76,6 +76,14 @@ def photo(img_url, img_name):
     img_folder = "C:/work/python/blog_API/static/"
     urllib.request.urlretrieve(img_url, img_folder+img_name)
 
+def save_photo_async(url, img_name):
+    def save_image():
+        img_folder = "C:/work/python/blog_API/static/"
+        urllib.request.urlretrieve(url, img_folder + img_name)
+    
+    thread = threading.Thread(target=save_image)
+    thread.start()
+
 ## 첫 번째 페이지
 @app.route('/')
 @app.route('/home')
@@ -85,113 +93,129 @@ def home():
 ## 두 번째 페이지
 @app.route('/place', methods=['GET', 'POST'])
 def place():
-    location = request.form.get('location', '') ## HTML에서 location값 받아옴
-    sel_rec = select(tb_res.name, tb_res.id).where(tb_res.district == f'{location}').order_by(tb_res.score.desc()).limit(5) 
-    restaurants = session.execute(sel_rec).all() ## 받아온 location과 같은 식당이름 저장
-    #print(restaurants)
+    try:
+        location = request.form.get('location', '')  # HTML에서 location값 받아옴
 
-    sel_cnt = session.query(
-    tb_res.district,
-    func.sum(tb_res.total_post_cnt),
-    func.sum(tb_res.post_cnt)
-    ).group_by(tb_res.district).subquery()
+        # 트랜잭션을 시작하여 연결을 얻어옴
+        with engine.begin() as conn:
+            sel_rec = select(tb_res.name, tb_res.id).where(tb_res.district == f'{location}').order_by(tb_res.score.desc()).limit(5) 
+            result = conn.execute(sel_rec)
+            restaurants = result.fetchall()  # 결과를 리스트로 저장
 
-    result = session.query(sel_cnt).all()
+        sel_cnt = session.query(
+            tb_res.district,
+            func.sum(tb_res.total_post_cnt),
+            func.sum(tb_res.post_cnt)
+        ).group_by(tb_res.district)
 
-    result_dict = {district: [int(total_post_cnt), int(post_cnt)] for district, total_post_cnt, post_cnt in result}
-    non_ad_rate = round(result_dict[location][1] / result_dict[location][0], 3) * 100
-    ad_rate = 100 - non_ad_rate
+        result = sel_cnt.all()
 
-    ## Pie
-    pie_chart = pygal.Pie(inner_radius=.4, style=custom_style)
-    pie_chart.title = '광고 비율'
-    pie_chart.add('AD', ad_rate)
-    pie_chart.add('NON-AD', non_ad_rate)
-    pie_chart = pie_chart.render_data_uri()
+        result_dict = {district: [int(total_post_cnt), int(post_cnt)] for district, total_post_cnt, post_cnt in result}
+        non_ad_rate = round(result_dict[location][1] / result_dict[location][0], 3) * 100
+        ad_rate = 100 - non_ad_rate
 
-    ## WordCloud
-    sel_dic = select(tb_words.key_words).where(tb_words.district == f'{location}')
-    data2 = session.execute(sel_dic).all()
-    data = data2[0][0]
-    data_str = data.replace("'", "\"")
-    data_dict = json.loads(data_str)
-    #print(type(data_dict))
-    
-    wordcloud = WordCloud(
-        font_path='malgun',
-        width=400,
-        height=300,
-        background_color='white'
-    ).generate_from_frequencies(data_dict)
+        ## Pie
+        pie_chart = pygal.Pie(inner_radius=.4, style=custom_style)
+        pie_chart.title = '광고 비율'
+        pie_chart.add('AD', ad_rate)
+        pie_chart.add('NON-AD', non_ad_rate)
+        pie_chart = pie_chart.render_data_uri()
 
-    img_buffer = io.BytesIO()
-    wordcloud.to_image().save(img_buffer, format='PNG')  # WordCloud를 이미지로 저장
+        ## WordCloud
+        sel_dic = select(tb_words.key_words).where(tb_words.district == f'{location}')
+        data2 = session.execute(sel_dic).all()
+        data = data2[0][0]
+        data_str = data.replace("'", "\"")
+        data_dict = json.loads(data_str)
 
-    img_buffer.seek(0)
-    img_data = base64.b64encode(img_buffer.read()).decode('utf-8')
+        wordcloud = WordCloud(
+            font_path='malgun',
+            width=400,
+            height=300,
+            background_color='white'
+        ).generate_from_frequencies(data_dict)
 
-    return render_template('page2.html', name=location, restaurants=restaurants, pie_chart=pie_chart, img_path=img_data)
+        img_buffer = io.BytesIO()
+        wordcloud.to_image().save(img_buffer, format='PNG')  # WordCloud를 이미지로 저장
+
+        img_buffer.seek(0)
+        img_data = base64.b64encode(img_buffer.read()).decode('utf-8')
+
+        return render_template('page2.html', name=location, restaurants=restaurants, pie_chart=pie_chart, img_path=img_data)
+
+    except Exception as e:
+        print("An error occurred:", e)
+        session.rollback()  # 트랜잭션 롤백
+        return render_template('error.html', error_message="An error occurred.")
+
+    finally:
+        session.close()  # 세션 닫기
 
 ## 세 번째 페이지
 @app.route('/restaurant', methods=['GET', 'POST'])
 def restaurant():
-    res = request.form.get('restaurant', '')  
+    try:
+        clicked_restaurant = request.args.get('restaurant', '')  
+        restaurant_info = clicked_restaurant.strip('()').split(', ')
+        restaurant_name = restaurant_info[0]
+        restaurant_id = int(restaurant_info[1])
 
-    clicked_restaurant = request.args.get('restaurant', '')  
-    restaurant_info = clicked_restaurant.strip('()').split(', ')
-    restaurant_name = restaurant_info[0]
-    restaurant_id = int(restaurant_info[1])
-    
-    #print(restaurant_name)
-    #print(restaurant_id)
+        # 트랜잭션을 시작하여 연결을 얻어옴
+        with engine.begin() as conn:
+            sel_top5_post = select(tb_post.title, tb_post.url, tb_post.description, tb_post.food_img_url).where(tb_post.res_id == f'{restaurant_id}').limit(30)
+            post_result = conn.execute(sel_top5_post)
+            post = post_result.fetchall()  # 결과를 리스트로 저장
 
-    sel_top5_post = select(tb_post.title, tb_post.url, tb_post.description, tb_post.food_img_url).where(tb_post.res_id == f'{restaurant_id}').limit(30)
-    post = session.execute(sel_top5_post).all()
+        l_post = [[],[],[],[]]
 
-    l_post = [[],[],[],[]]
-    
-    for i in range(len(post)):
-        try:
-            l_post[0].append(post[i][0])
-            l_post[1].append(post[i][1])
-            l_post[2].append(post[i][2])
-            url = post[i][3]
-            img = f'img{i}.jpg'
-            l_post[3].append(img)
-            photo(url, img)
-        except Exception as e:
-            #print(f"Error while processing row {i}: {e}")
-            continue  # 오류가 발생한 행을 건너뛰고 다음 행으로 이동합니다.
-    
-    ## 메뉴, 개수 가져오기
-    sel_menu = select(tb_menu.menu, tb_menu.menu_cnt).where(tb_menu.res_id == f'{restaurant_id}').order_by(tb_menu.menu_cnt.desc()).limit(5)
-    menu = session.execute(sel_menu).all()
+        for i in range(len(post)):
+            try:
+                l_post[0].append(post[i][0])
+                l_post[1].append(post[i][1])
+                l_post[2].append(post[i][2])
+                url = post[i][3]
+                img = f'img{i}.jpg'
+                l_post[3].append(img)
+                save_photo_async(url, img)  # 이미지 저장을 비동기적으로 실행
+            except Exception as e:
+                continue
+        
+        ## 메뉴, 개수 가져오기
+        sel_menu = select(tb_menu.menu, tb_menu.menu_cnt).where(tb_menu.res_id == f'{restaurant_id}').order_by(tb_menu.menu_cnt.desc()).limit(5)
+        menu = session.execute(sel_menu).all()
 
-    ## Bar
-    bar_chart = pygal.HorizontalBar(style=custom_style)
-    bar_chart.title = '메뉴 언급 수'
-    bar_chart.legend_at_bottom = True
-    for i in range(len(menu)):
-        bar_chart.add(menu[i][0], menu[i][1])
-        #bar_chart.add(menu[i][0], [{'value': menu[i][1], 'label': str(menu[i][1])}])
-    bar_chart.x_label_rotation = (360 - 45)
-    bar_chart = bar_chart.render_data_uri()
+        ## Bar
+        bar_chart = pygal.HorizontalBar(style=custom_style)
+        bar_chart.title = '메뉴 언급 수'
+        bar_chart.legend_at_bottom = True
+        for i in range(len(menu)):
+            bar_chart.add(menu[i][0], menu[i][1])
+        bar_chart.x_label_rotation = (360 - 45)
+        bar_chart = bar_chart.render_data_uri()
 
-    ## 분기별 포스팅 수 받아오기
-    sel_p_cnt = select(tb_p_cnt.res_id, tb_p_cnt.year_q, tb_p_cnt.post_count).where(tb_p_cnt.res_id == f'{restaurant_id}').order_by(tb_p_cnt.year_q)
-    p_cnt = session.execute(sel_p_cnt).all()
+        ## 분기별 포스팅 수 받아오기
+        sel_p_cnt = select(tb_p_cnt.res_id, tb_p_cnt.year_q, tb_p_cnt.post_count).where(tb_p_cnt.res_id == f'{restaurant_id}').order_by(tb_p_cnt.year_q)
+        p_cnt = session.execute(sel_p_cnt).all()
 
-    ## Line
-    date_chart = pygal.Line(x_label_rotation=20, style=custom_style)
-    date_chart.title = '기간별 포스팅 수'
-    date_chart.x_title = "날짜(년도-분기)"
-    x_labels = [item[1] for item in p_cnt]
-    values = [item[2] for item in p_cnt]
-    date_chart.x_labels = x_labels
-    date_chart.add('Posts', values) 
-    date_chart = date_chart.render_data_uri()
+        ## Line
+        date_chart = pygal.Line(x_label_rotation=20, style=custom_style)
+        date_chart.title = '기간별 포스팅 수'
+        date_chart.x_title = "날짜(년도-분기)"
+        x_labels = [item[1] for item in p_cnt]
+        values = [item[2] for item in p_cnt]
+        date_chart.x_labels = x_labels
+        date_chart.add('Posts', values) 
+        date_chart = date_chart.render_data_uri()
 
-    return render_template('page3.html', restaurant=restaurant_name, l_post=l_post, bar_chart=bar_chart, date_chart=date_chart)
+        return render_template('page3.html', restaurant=restaurant_name, l_post=l_post, bar_chart=bar_chart, date_chart=date_chart)
+
+    except Exception as e:
+        print("An error occurred:", e)
+        session.rollback()  # 트랜잭션 롤백
+        return render_template('error.html', error_message="An error occurred.")
+
+    finally:
+        session.close()  # 세션 닫기
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, port=8000)
